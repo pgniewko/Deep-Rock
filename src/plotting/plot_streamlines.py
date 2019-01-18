1509a079475102c2bce9add728c955b9fd67a22c
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-01-17 17:12:44 -0800

    Figured plotting streamlines

--- src/plotting/plot_streamlines.py
+++ src/plotting/plot_streamlines.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 
-
-
 import sys
 import numpy
 import numpy as np
@@ -9,57 +7,58 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
-data = np.loadtxt(sys.argv[1])
+def format_axes(ax):
+    ax.set_aspect('equal')
+    ax.figure.subplots_adjust(bottom=0, top=1, left=0, right=1)
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticks([])
+    for spine in ax.spines.itervalues():
+        spine.set_visible(False)
 
-Xo, Yo, UX, UY = data.T
 
-print UX[0], Xo[0]
-print len(UX)
+if __name__ == "__main__":
+    data = np.loadtxt(sys.argv[1])
 
-LX = 64
-LY = 192
+    Xo, Yo, UX, UY, U = data.T
 
-Y, X = np.mgrid[0:LX, 0:LY]
+    Xo += 0.5
+    Yo += 0.5
+    LX = 64
+    LY = 192
 
-U = np.zeros( [LX, LY] )
-V = np.zeros( [LX, LY] )
+    Y, X = np.mgrid[0:LX, 0:LX]
 
-mask = np.zeros(U.shape, dtype=bool)
+    U = np.zeros( [LX, LX] )
+    V = np.zeros( [LX, LX] )
 
-print U.shape
-for i in range( len( Xo ) ):
-    xi = int(Xo[i])
-    yi = int(Yo[i])
-    U[yi][xi] = UX[i]
-    V[yi][xi] = UY[i]
+    mask = np.zeros(U.shape, dtype=bool)
 
-    if UX[i] == 0 and UY[i] == 0:
-        mask[63-yi][xi] = True
+    for i in range( len( Xo ) ):
+        xi = int(Xo[i])
+        yi = int(Yo[i])
+        if xi >= LX and xi < 2*LX:
+            U[yi][xi-LX] = UX[i]
+            V[yi][xi-LX] = UY[i]
+    
+        if UX[i] == 0 and UY[i] == 0:
+            mask[63-yi][xi-LX] = True
 
-print type(Y)
-print type(U)
-print X.shape
-print Y.shape
-print U.shape
-print V.shape
+    speed = np.sqrt(U*U + V*V)
+    lw = 5*speed / speed.max()
 
-speed = np.sqrt(U*U + V*V)
-lw = 5*speed / speed.max()
+    zero_v = lw < 0.15
+    U[zero_v] = 0.0
+    V[zero_v] = 0.0
 
-zero_v = lw < 0.5
-U[zero_v] = 0.0
-V[zero_v] = 0.0
+    fig, ax1 = plt.subplots(1, 1, sharey=True, figsize=(6,6) )
+    format_axes(ax1)
 
-fig, ax1 = plt.subplots(1, 1, sharey=True, figsize=(6,2) )
-ax1.streamplot(X, Y, U, V, density=[10,10]) #, linewidth=lw)
+    ax1.streamplot(X, Y, U, V, density=5, arrowsize=0.01, linewidth=lw)
 
-ax1.set_title('Varying Density')
-ax1.scatter([10,10],[10,10],color='red')
+    ax1.imshow(~mask, extent=(0-0.5, 64-0.5, 0-0.5, 64-0.5), alpha=0.45,
+                interpolation='nearest', cmap='gray', aspect='auto')
 
-w=2
-ax1.imshow(~mask, extent=(0, 192, 0, 64), alpha=0.5,
-          interpolation='nearest', cmap='gray', aspect='auto')
-ax1.set_aspect('equal')
+    ax1.set_aspect('equal')
 
-plt.tight_layout()
-plt.show()
+    #plt.tight_layout()
+    plt.show()
