4c4ff51ea89b07872785c131e866cce64c867748
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-07-14 22:52:21 -0700

    New version of the writeup

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #! /bin/bash -x 
+#
 
-OPATH="/Users/pawel/Desktop/LATTICE/"
-SF="./src/lattice/mc.py"
+OUTPUT_PATH="/Users/pawel/Desktop/LATTICE/"
+PERCO="./src/lattice/mc.py"
 
 for L in 64 128; do
   for k in 2 4; do
     for INDEX in `seq 1 10000`; do
-      $SF $L $k $INDEX $OPATH
+      $PERCO $L $k $INDEX $OUTPUT_PATH
     done
   done
 done
