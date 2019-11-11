41ec959d52387beda8d488af165e116596d0546a
Pawel Gniewek <gniewko.pablo@gmail.com>
2019-11-11 12:55:25 -0800

    Doc strings

--- src/cnn/make_list.sh
+++ src/cnn/make_list.sh
@@ -1,9 +1,12 @@
 #! /bin/bash -x
+# Create a list of files that are needed for the CNN input preparation script.
+# In the following, we generate a list of files for L=64 and k=2.
+# Refer to the paper for the details.
 
 LATTICE="/Users/pawel/Projects/Deep-Rock/output/LATTICE"
 LB_PATH="/Users/pawel/Projects/Deep-Rock/output/LB"
 
-rm FILES_LIST.txt
+rm FILES_LIST.txt 2> /dev/null
 
 for INDEX in `seq 1 10000`; do
 
